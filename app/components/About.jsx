f0bca5d5f7cafd55224b16ea38df931f518fac18
rjdp2 <rajdeep.sanyal3@gmail.com>
2016-10-27 16:32:45 +0530

    Style about page and link to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,11 +1,26 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var About = React.createClass({
     render: function () {
         return (
             <div>
-            <h3>About</h3>
+            <h1 classname="text-center">About</h1>
             <p>Welcome to the abouts page</p>
+            <div className="row">
+                <div className="columns medium-6 large-4 small-centered">
+                    <p>Tools used</p>
+                </div>
+            </div>
+            <div className="row">
+                <div className="columns medium-6 large-4 small-centered">
+                    <ol>
+                        <li><a href='https://facebook.github.io/react/docs/hello-world.html'>Reactjs</a></li>
+                        <li><a href='http://openweathermap.org/'>OpenWeatherMap</a></li>
+                    </ol>
+                </div>
+            </div>
+
             </div>
         );
     }
